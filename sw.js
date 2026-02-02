c8ee61b7fe9703b3507fdc4647d1f6c44a0a74c6
mnlznn <manuelzannier@icloud.com>
2026-02-02 15:57:37 +0100

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,22 @@
+
+const CACHE_NAME = 'ristoranti-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon192.png',
+  './icon512.png'
+];
+self.addEventListener('install', (e) => {
+  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
+});
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.map(k => k!==CACHE_NAME && caches.delete(k))))
+  );
+});
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then(res => res || fetch(e.request).then(resp => resp).catch(() => caches.match('./index.html')))
+  );
+});
